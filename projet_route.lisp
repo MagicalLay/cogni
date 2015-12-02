cb86a9e3ce504db6754751cd8e08fb121992affc
PMAiriau <pierre-marie.airiau@hotmail.fr>
2015-12-01 20:28:10 -0500

    creation chunks + reconnaissance environnement + obtention couleur feu via envrionnement

--- projet_route.lisp
+++ projet_route.lisp
@@ -5,19 +5,30 @@
   (reset)
   
   
-  (let* ((lis (permute-list '("R" "V")))
-         (text1 (first lis))
+  (let* ((feu (permute-list '("R" "V" "non")))
+		 (route (permute-list '("r1" "r2")))
+		 (climat (permute-list '("cl" "cl2")))
+		 (passage (permute-list '("oui" "non")))
+		 (envir (permute-list '("env1" "env2" "env3")))
+		 (voiture (permute-list '("Voit1" "Voit2" "Voit3")))
+		 
+         (text1 (first feu))
+		 (text2 (first route))
+		 (text3 (first climat))
+		 (text4 (first passage))
+		 (text5 (first voiture))
+		 (text6 (first envir))
          (window (open-exp-window "Environnement")))
     
-    (add-text-to-exp-window :text text1 :x 125 :y 150)
+    (add-text-to-exp-window :text text6 :x 125 :y 150)
+	;;(add-text-to-exp-window :text text2 :x 125 :y 150)
+	;;(add-text-to-exp-window :text text3 :x 125 :y 150)
+	
    
     (install-device window)
     (proc-display)
     (run 10 :real-time t))      
   )
-
-
-
 (clear-all)
 
 (define-model projet_route
@@ -29,23 +40,74 @@
 ;;ans : Activation Noise S => bruit ajouté aux chunks activés à chaque cycle
 	(sgp :esc t :rt -.45 :v t :ans 0.5)
     
- (chunk-type Vehicule Type SendDeCirculation Distance Vitesse NumeroDeVoie Phares)
- (chunk-type Environnement Route Climat PassagePieton Feu)
- (chunk-type Route NombreDeVoies NombreSens)
- (chunk-type Climat Nuit Visibilite Gel)
+	
+	
+	
+;; Tous les chunk-types, exceptés ceux à un attribut, ont comme attribut "nom" qui sert à les trouver
+	
+;;différentes valeurs voiture ===>
+;;type : "normal", "electrique", "moto"
+;;sensdecirculation : "gauche", "droite" (on s'interesse seulement aux vehicules venant vers nous)
+;;distance/vitesse : "grande", "moyenne", "petite"
+;;numerodevoie : (integer)
+;;phares : "E", "A"
+ (chunk-type Vehicule nomV Type SendDeCirculation Distance Vitesse NumeroDeVoie Phares)
+ 
+;;différentes valeurs environnement ===>
+;;route : (chunk type)
+;;climat : (chunk type)
+;;passagepieton : "oui", "non"
+;;feu : (chunk type), "non"
+ (chunk-type Environnement nomE Route Climat PassagePieton Feu)
+ 
+;;différentes valeurs route ===>
+;;NombreDeVoies : (integer)
+;;NombreSens : "un", "deux"
+ (chunk-type Route nomR NombreDeVoies NombreSens)
+ 
+;;différentes valeurs Climat ===>
+;;Nuit : "jour", "nuit"
+;;Visibilite : "bonne, "mauvaise"
+;;gel : "oui", "non"
+ (chunk-type Climat nomC Nuit Visibilite Gel)
+ 
+;;différentes valeurs Feu ===>
+;;Couleur : "vert", "rouge"
  (chunk-type Feu Couleur)
+ 
+;;différentes valeurs Decision ===>
+;;state : "stop", "courir", "marcher"
  (chunk-type Decision state)
+ 
+;;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+;;Ces valeurs peuvent être modifié DU MOMENT QUE vous modifiez le code pour gérer ces modifications !
+;;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
 
 (add-dm
-;(Voiture1 ISA Vehicule Type "normal" SendDeCirculation "gauche" Distance 80 Vitesse 50 NumeroDeVoie 1 Phares "E")
-;(Env1 ISA Environnement Route Route1 Climat Climat1 PassagePieton "oui" Feu Feu1)
-;(Route1 ISA Route NombreDeVoies 1 NombreSens 1)
-;(Climat1 ISA Climat Nuit "non" Visibilite "ok" Gel "non")
-;(Feu1 ISA Feu Couleur "vert")
-(start ISA chunk) (attend ISA chunk)
+(Voiture1 ISA Vehicule nomV "voiture1" Type "normal" SendDeCirculation "gauche" Distance "grande" Vitesse "grande" NumeroDeVoie 1 Phares "E")
+(Voiture2 ISA Vehicule nomV "voiture2" Type "normal" SendDeCirculation "droite" Distance "moyenne" Vitesse "grande" NumeroDeVoie 1 Phares "A")
+(Voiture3 ISA Vehicule nomV "voiture3" Type "electrique" SendDeCirculation "gauche" Distance "petite" Vitesse "moyenne" NumeroDeVoie 1 Phares "E")
+
+(Env1 ISA Environnement nomE "env1" Route Route1 Climat Climat1 PassagePieton "non" Feu "non")
+(Env2 ISA Environnement nomE "env2" Route Route2 Climat Climat2 PassagePieton "oui" Feu "rouge")
+(Env3 ISA Environnement nomE "env3" Route Route2 Climat Climat2 PassagePieton "oui" Feu "vert")
+
+(Route1 ISA Route nomR "route1" NombreDeVoies 1 NombreSens "un")
+(Route2 ISA Route nomR "route2" NombreDeVoies 1 NombreSens "deux")
+
+(Climat1 ISA Climat nomC "climat1" Nuit "non" Visibilite "bonne" Gel "non")
+(Climat2 ISA Climat nomC "climat2" Nuit "oui" Visibilite "bonne" Gel "non")
+
+(Feu1 ISA Feu Couleur "vert")
+(Feu2 ISA Feu Couleur "rouge")
+(Feu3 ISA Feu Couleur "non")
+
+(start ISA chunk) (attend ISA chunk) (researchFeu ISA chunk)
  (respond ISA chunk) (done ISA chunk) (finFEU ISA chunk)
-(goal ISA Decision state start)
-)
+(goal ISA Decision state start))
 
 (P start-environnement
    =goal>
@@ -65,10 +127,8 @@
       state       find-location
    =visual-location>
       ISA         visual-location
-   
    ?visual>
       state       free
-   
 ==>
    +visual>
       ISA         move-attention
@@ -77,6 +137,23 @@
       state       attend
 )
 
+
+(P save-env
+   =goal>
+      ISA         Decision
+      state       attend
+   =visual>
+      ISA         text
+      value       =name
+==>
+   =goal>
+      state       researchFeu
+   +retrieval>
+      isa         Environnement
+      nomE      =name
+)
+
+#|
 (P save-env
    =goal>
       ISA         Decision
@@ -92,8 +169,25 @@
    +imaginal>
       isa         Feu
       Couleur      =letter
-)
+)|#
 
+
+(P researchEnv
+   =goal>
+      ISA         Decision
+      state       researchFeu
+   =retrieval>
+      isa         Environnement
+      Feu			=fire
+   ?imaginal>
+      state       free
+==>
+   =goal>
+      state       respond
+   +imaginal>
+      isa         Feu
+      Couleur      =fire
+)
 
 (P respond1
    =goal>
@@ -101,7 +195,7 @@
       state       respond
    =imaginal>
       isa         Feu
-      Couleur      "v"
+      Couleur      "vert"
 ==>
    =goal>
       state       done
@@ -114,19 +208,28 @@
       state       respond
    =imaginal>
       isa         Feu
-      Couleur      "r"
+      Couleur      "rouge"
 ==>
    =goal>
       state       done
    !output!       (Le feu est rouge!)
 )
 
+(P stop-noFeu
+   =goal>
+      ISA         Decision
+      state       respond
+   =imaginal>
+      ISA         Feu
+      Couleur       "non"
+==>
+   =goal>
+      state       done
+   !output!       (Il n'y a pas de feu!)
+)
+
 
 
 (goal-focus goal)
 
-
-
-
 )
-
